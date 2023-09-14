206a362a29861c19012b8b15f86ec6302b065fbf
3DSB - ETEC Jardim ângela <144622827+3DsbJa@users.noreply.github.com>
2023-09-14 20:55:03 -0300

    Update index.js

--- main/controller/js/index.js
+++ main/controller/js/index.js
@@ -1,6 +1,7 @@
 const navbar = document.getElementById("navBar")
 function ativarMenu(){
     if(document.getElementById('ativar-menu').checked){
+        navbar.style.visibility = "visible";
         navbar.style.marginLeft = "50vw"
     }else{
         navbar.style.marginLeft = "150vw"
